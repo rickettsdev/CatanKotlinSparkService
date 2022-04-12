5f747df1bccf75ca85c456a8cd210a3a01dde988
Thomas Ricketts <rickettsdev@gmail.com>
2022-04-11 20:25:45 -0400

    Added test for piece scarcity.

--- src/test/kotlin/software/parable/constraints/CatanGamePieceConstraintManagerTest.kt
+++ src/test/kotlin/software/parable/constraints/CatanGamePieceConstraintManagerTest.kt
@@ -3,6 +3,7 @@ package software.parable.constraints
 import gameDataObjects.types.CatanColor
 import gameDataObjects.types.CatanResource
 import software.parable.services.catan.constraints.CatanGamePieceConstraintManager
+import java.lang.Exception
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -66,5 +67,65 @@ internal class CatanGamePieceConstraintManagerTest {
         assertEquals(expectedCardCount, constraintManager.resourceCardsAvailable.woodCount)
         assertEquals(expectedSheepCount, constraintManager.resourceCardsAvailable.sheepCount)
         assertEquals(expectedCardCount, constraintManager.resourceCardsAvailable.stoneCount)
+    }
+
+    @Test
+    fun redPlayerCanBuildSettlement() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.BRICK, CatanResource.WOOD, CatanResource.SHEEP, CatanResource.WHEAT))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.settlementPieceCount = 0
+        // when
+        constraintManager.playerBuildRoad(CatanColor.RED)
+        // then no exception is thrown
+    }
+
+    @Test
+    fun redPlayerCanBuildCity() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.WHEAT, CatanResource.WHEAT, CatanResource.WHEAT, CatanResource.STONE, CatanResource.STONE))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.cityPieceCount = 0
+        // when
+        constraintManager.playerBuildCity(CatanColor.RED)
+        // then no exception is thrown
+    }
+
+    @Test
+    fun redPlayerCanBuildRoad() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.WOOD, CatanResource.BRICK))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.roadPieceCount = 0
+        // when
+        constraintManager.playerBuildRoad(CatanColor.RED)
+        // then no exception is thrown
+    }
+
+    @Test(expected = Exception::class)
+    fun bluePlayerCannotBuildCity() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.WHEAT, CatanResource.WHEAT, CatanResource.WHEAT, CatanResource.STONE, CatanResource.STONE))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.cityPieceCount = 0
+        // when
+        constraintManager.playerBuildCity(CatanColor.BLUE)
+        // then exception is thrown
+    }
+
+    @Test(expected = Exception::class)
+    fun bluePlayerCannotBuildRoad() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.WOOD, CatanResource.BRICK))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.roadPieceCount = 0
+        // when
+        constraintManager.playerBuildRoad(CatanColor.BLUE)
+        // then exception is thrown
+    }
+
+    @Test(expected = Exception::class)
+    fun bluePlayerCannotBuildSettlement() {
+        // given
+        constraintManager.playerReceivedTheseCards(listOf(CatanResource.BRICK, CatanResource.WOOD, CatanResource.SHEEP, CatanResource.WHEAT))
+        constraintManager.playerPieceState[CatanColor.BLUE]!!.settlementPieceCount = 0
+        // when
+        constraintManager.playerBuiltSettlement(CatanColor.BLUE)
+        // then exception is thrown
     }
 }
